adf4a21c74f8bb606260c9c0b5a8e311a772c40e
dev-tes <tesleem.amuda@gmail.com>
2021-11-03 08:57:16 +0100

    create project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp());
+}
+
+class MaterialApp extends StatelessWidget {
+  const MaterialApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
